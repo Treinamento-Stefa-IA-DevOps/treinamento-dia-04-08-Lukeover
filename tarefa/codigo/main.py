702df6befdaa22263944016ca5c8606991552399
Lukas Barbosa de Oliveira <lukas.oliveira4@outlook.com>
2021-08-05 22:14:48 -0300

    Correção do dockerfile e main.py - Resposta da tarefa

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,12 +1,20 @@
 import pickle
-from fastapi import FastAPI
+from fastapi import FastAPI ,status
 
 app = FastAPI()
-@app.post('/model')
+@app.post('/model', status_code=200)
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+def titanic(Sex:int,Age:float,Lifeboat:int,Pclass:int):
+    pred = ([[Sex,Age,Lifeboat,Pclass]])
     with open('model/Titanic.pkl', 'rb') as fid: 
+        print(pred)
         titanic = pickle.load(fid)
+        answer = bool(titanic.predict(pred))
+        m = "Azar,Você não morreria no titanic :("
+        if answer: 
+            m = "Parabéns você sobreviveria ao titanic!"
+        status_code = status.HTTP_200_CREATED
+    return {"survived": answer,"status": status_code,"message": m}
 
 @app.get('/model')
 def get():
